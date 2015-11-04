9cf34eb3204009a77f36c0081d721120cd07fd6d
Michael D. Hoyle <hoylemd@gmail.com>
2015-11-04 17:30:52 -0500

    add scalar class

---
+++ game/scalar.py
@@ -0,0 +1,33 @@
+
+class Scalar(object):
+    def __init__(self, current_value, max_value=None, min_value=None):
+        if max_value is None:
+            self.max_value = float(current_value)
+        else:
+            self.max_value = float(max_value)
+
+        if min_value is None:
+            self.min_value = float(0)
+        else:
+            self.min_value = float(min_value)
+
+        if self.max_value < self.min_value:
+            self.min_value, self.max_value = self.max_value, self.min_value
+
+        self.set_value(current_value)
+
+    def set_value(self, value):
+        if value > self.max_value:
+            value = self.max_value
+
+        if value < self.min_value:
+            value = self.min_value
+
+        self.current = float(value)
+        self._calc_fraction()
+
+    def _calc_fraction(self, value):
+        numerator = value - self.min_value
+        denominator = self.max_value - self.min_value
+
+        self.fraction = numerator / denominator
